f7185c4fa541401b87dc13e16cc5129eff787ea6
gilbertKorir <jerikorir@gmail.com>
2022-04-23 12:25:51 +0300

    started a rectangle project

---
+++ src/main/java/App.java
@@ -0,0 +1,39 @@
+//package models;//import rectangle.java.recatangle;
+//import java.util.Scanner;
+import java.util.Scanner;
+import java.awt.*;
+import java.io.Console;
+import java.util.Scanner;
+
+//
+//Scanner in;
+//in = new Scanner(System.in);
+//String s = in.nextLine();
+public class App {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+        //For lenght
+        System.out.println("Enter the length (or 'height') of your rectangle:");
+
+
+
+        Scanner in;
+        in = new Scanner(System.in);
+        String stringLenght = in.nextLine();
+
+//        String stringLenght = myConsole.readLine();
+        int intLenght = Integer.parseInt(stringLenght);
+        //for width
+        System.out.println("Enter the width of your rectangle:");
+        String stringWidth = myConsole.readLine();
+        int intWidth = Integer.parseInt(stringWidth);
+        //create an instance of the class rectangle;
+
+        Rectangle rectangle = new Rectangle(intLenght, intWidth);
+
+//         call isSquare() to check if it is also a square
+        boolean squareResult = rectangle.isSquare();
+//        System.out.println("Is your rectangle a square, too?" + squareResult + "!");
+
+    }
+}
